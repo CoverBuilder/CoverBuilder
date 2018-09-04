f36ee180ca387a3e3a7b6d5089d2f9cbc7ac9d3c
Bruno Herfst <hello@brunoherfst.com>
2018-09-04 15:26:09 +1000

    Always send attached colorProfile to Photoshop
    Regardless what is saved in C object. Things might have been changed.

--- Installer/CoverBuilder/CoverBuilder_Startup_Menu.jsx
+++ Installer/CoverBuilder/CoverBuilder_Startup_Menu.jsx
@@ -22,7 +22,7 @@
 
 #targetengine "session"
 
-var appVersion = "3.0.8";
+var appVersion = "3.0.9";
 
 // While most localised strings live at CoverBuilder.Localise
 // These are some strings needed before we the plugin is loaded
